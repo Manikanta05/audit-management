579636eeb88a0c602cbbdb6ba88f583941f11c91
Akansha0211 <akansha.saxena0211@gmail.com>
2022-08-02 23:41:23 +0530

    Changing to more precise variable name

--- Audit-Benchmark-microservice/src/main/java/com/cts/audit/pojo/AuditBenchmark.java
+++ Audit-Benchmark-microservice/src/main/java/com/cts/audit/pojo/AuditBenchmark.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/*
+ * THis POJO deals with auditType and acceptable no of questions whose answer is No
+ */
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuditBenchmark {
 	private String auditType;
-	private Integer noAnswers;
+	private Integer accNoAnswers;
 }
