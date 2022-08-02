9252e7cdd70642190131e05f5226d73748757e5e
Akansha0211 <akansha.saxena0211@gmail.com>
2022-08-02 23:06:22 +0530

    Added POJO AuditType

--- Audit-Benchmark-microservice/src/main/java/com/cts/audit/pojo/AuditType.java
+++ Audit-Benchmark-microservice/src/main/java/com/cts/audit/pojo/AuditType.java
@@ -1,5 +1,14 @@
 package com.cts.audit.pojo;
 
-public class AuditType {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuditType {
+	private String auditType;
 }
