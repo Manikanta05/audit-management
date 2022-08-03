2c991aee7691a3b37a2e06d3f388f7f67b6e7f9f
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2022-08-03 07:58:54 +0530

    updated runner class

--- Audit-Severity-microservice/src/main/java/com/cts/audit/severity/AuditSeverityMicroserviceApplication.java
+++ Audit-Severity-microservice/src/main/java/com/cts/audit/severity/AuditSeverityMicroserviceApplication.java
@@ -2,7 +2,11 @@ package com.cts.audit.severity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages= {"com.cts.audit.severity.repository"})
+@EntityScan(basePackages={"com.cts.audit.severity.model"})
 @SpringBootApplication
 public class AuditSeverityMicroserviceApplication {
 
