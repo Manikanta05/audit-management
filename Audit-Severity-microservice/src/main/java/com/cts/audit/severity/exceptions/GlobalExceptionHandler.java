a09ee09ecf283304122f3fa0f8f570f58e5c6195
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2022-08-04 12:01:11 +0530

    added exceptions

---
+++ Audit-Severity-microservice/src/main/java/com/cts/audit/severity/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.cts.audit.severity.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Global Exception Handler class
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(TokenNotValidException.class)
+	public ResponseEntity<CustomErrorResponse> handleTokenExpiredEception(TokenNotValidException tokenNotValidException){
+		CustomErrorResponse response = new CustomErrorResponse();
+		response.setDateTime(LocalDateTime.now());
+		response.setMessage(tokenNotValidException.getMessage());
+		return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+	}
+}
