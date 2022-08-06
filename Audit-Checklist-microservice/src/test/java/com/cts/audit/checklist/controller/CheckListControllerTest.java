e1883aa2b4836739972808c73ed8407ddbb7460b
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2022-08-06 19:33:12 +0530

    Update CheckListControllerTest.java

--- Audit-Checklist-microservice/src/test/java/com/cts/audit/checklist/controller/CheckListControllerTest.java
+++ Audit-Checklist-microservice/src/test/java/com/cts/audit/checklist/controller/CheckListControllerTest.java
@@ -2,8 +2,9 @@ package com.cts.audit.checklist.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,77 +20,76 @@ import org.springframework.test.context.ContextConfiguration;
 
 import com.cts.audit.checklist.exceptions.TokenExpiredException;
 import com.cts.audit.checklist.feign.AuthenticationFeign;
-
 import com.cts.audit.checklist.pojo.AuditType;
 import com.cts.audit.checklist.repository.QuestionsRepository;
 import com.cts.audit.checklist.services.QuestionsServiceImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
-
-
-
 @Slf4j
-@ContextConfiguration 
+@ContextConfiguration
 @SpringBootTest
 public class CheckListControllerTest {
 
 	@Mock
 	AuthenticationFeign authClient;
-	
-	
+
 	@Mock
 	AuditType auditType;
-	
+
 	@Mock
 	QuestionsServiceImpl questionsService;
-	
+
 	@Mock
 	Environment env;
-	
+
 	@InjectMocks
 	CheckListController auditCheckListController;
-	
+
 	@Mock
 	QuestionsRepository questionsRepository;
-	
+
 	@Mock
 	TokenExpiredException tokenexp;
-	
+
 	@Test
 	public void contextLoads() {
 		assertNotNull(auditCheckListController);
 	}
+
 	/**
 	 * To test testGetCheckList method
 	 */
 	@Test
 	public void testGetQuestions() {
 		ResponseEntity<?> responseEntity = null;
-		ResponseEntity<Boolean> valid=new ResponseEntity<Boolean>(true,HttpStatus.OK);
+		ResponseEntity<Boolean> valid = new ResponseEntity<Boolean>(true, HttpStatus.OK);
 		List<String> questionsList = new ArrayList<String>();
 		questionsList.add("How are you");
 		when(authClient.validate("token")).thenReturn(valid);
 		when(questionsService.getQuestions("Internal")).thenReturn(questionsList);
-		responseEntity = auditCheckListController.getQuestions("token","Internal");
-		assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+		responseEntity = auditCheckListController.getQuestions("token", "Internal");
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 		log.info(responseEntity.getBody().toString());
-		assertEquals("[How are you]",responseEntity.getBody().toString());	
-			
+		assertEquals("[How are you]", responseEntity.getBody().toString());
+
 	}
-	
+
 	@Test
 	public void testGetQuestionsTokenInvalid() {
-		tokenexp=new TokenExpiredException("Token is expired");
-		ResponseEntity<Boolean> valid=new ResponseEntity<Boolean>(false,HttpStatus.INTERNAL_SERVER_ERROR);
-		List<String> questionsList = new ArrayList<String>();
-		questionsList.add("How are you");
-		when(authClient.validate("token")).thenReturn(valid);
-		when(questionsService.getQuestions("Internal")).thenReturn(questionsList);
-		when(auditCheckListController.getQuestions("token","Internal")).thenThrow(tokenexp);
-		assertEquals(new TokenExpiredException("Token is expired or Invalid token, please login again!"),auditCheckListController.getQuestions("token","Internal"));
-		
-			
+		Exception exception = assertThrows(TokenExpiredException.class, () -> {
+			ResponseEntity<Boolean> valid = new ResponseEntity<Boolean>(false, HttpStatus.BAD_REQUEST);
+			List<String> questionsList = new ArrayList<String>();
+			questionsList.add("How are you");
+			when(authClient.validate("token")).thenReturn(valid);
+			when(questionsService.getQuestions("Internal")).thenReturn(questionsList);
+			auditCheckListController.getQuestions("token", "Internal");
+		});
+
+		String expectedMessage = "Token is expired or Invalid token, please login again!";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+
 	}
 }
-	
