a1732b8e63c0aaaab171ecec3345cc652dd29398
Vanamala Manish <vanamalamanish4@gmail.com>
2022-08-10 16:11:36 +0530

    updated api url in feign client

--- Audit-Checklist-microservice/src/main/java/com/cts/audit/checklist/feign/AuthenticationFeign.java
+++ Audit-Checklist-microservice/src/main/java/com/cts/audit/checklist/feign/AuthenticationFeign.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 //${AUTH_SERVICE:http://localhost:8081/auth}
 
-@FeignClient(name="auth-service",url="CDE015-POD1-audit-auth-lb-2030192275.ap-south-1.elb.amazonaws.com/auth")
+@FeignClient(name="auth-service",url="cde015-pod1-audit-auth-lb-875635810.ap-south-1.elb.amazonaws.com/auth")
 public interface AuthenticationFeign {
 
 	
--- Audit-Severity-microservice/src/main/java/com/cts/audit/severity/feign/AuthenticationFeign.java
+++ Audit-Severity-microservice/src/main/java/com/cts/audit/severity/feign/AuthenticationFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-@FeignClient(name="auth-service",url="http://43.204.97.42:8081/auth")
+@FeignClient(name="auth-service",url="cde015-pod1-audit-auth-lb-875635810.ap-south-1.elb.amazonaws.com/auth")
 public interface AuthenticationFeign {
 
 	
